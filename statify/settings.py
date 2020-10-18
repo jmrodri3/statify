c1ebdc81ff33a152eb389c87a7d99b258aa97f11
diegoruv20 <diegoruv0@gmail.com>
2020-10-17 18:59:18 -0700

    move secret key to local secret_settings.py file

--- statify/settings.py
+++ statify/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from secret_settings import SecretKey
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w_zp0=a%iho)sc-fet$$4do=7omzf2kaz+=qk9&vj*no8e5(1$'
+SECRET_KEY = SecretKey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
